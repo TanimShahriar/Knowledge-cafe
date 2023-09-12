74f2ee9daef134022cbe359607fa8d406d10d314
TanimShahriar <ssmtsshr@gmail.com>
2023-09-12 06:16:32 +0600

    boilerplate setup completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+
+      <h1 className='text-4xl bg-green-500 text-white'>Knowledge Cafe</h1>
+
+
+
+    </>
+  )
+}
+
+export default App
